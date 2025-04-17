d026c656ac14b57072538577348253e56e183648
Ihtisham Mehmood <ahtasham.marral12@gmail.com>
2025-04-17 20:44:01 +0500

    Model serving with Fast API

---
+++ FastAPI-Model_Serving/client.py
@@ -0,0 +1,27 @@
+import streamlit as st
+import requests
+
+st.title("🌸 Iris Flower Prediction")
+
+st.markdown(
+    "Measurements are in**centimeters (cm)**"
+)
+
+sepal_length = st.slider("Sepal Length", 4.0, 8.0, 5.1)
+sepal_width = st.slider("Sepal Width", 2.0, 4.5, 3.5)
+petal_length = st.slider("Petal Length", 1.0, 7.0, 1.4)
+petal_width = st.slider("Petal Width", 0.1, 2.5, 0.2)
+
+if st.button("Predict"):
+    data = {
+        "sepal_length": sepal_length,
+        "sepal_width": sepal_width,
+        "petal_length": petal_length,
+        "petal_width": petal_width
+    }
+    response = requests.post("http://localhost:8000/predict", json=data)
+    if response.status_code == 200:
+        result = response.json()
+        st.success(f"Predicted Class: {result['prediction'].capitalize()}")
+    else:
+        st.error("Prediction failed. Check if FastAPI server is running.")
---
+++ FastAPI-Model_Serving/server.py
@@ -0,0 +1,34 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import joblib
+import numpy as np
+from sklearn.datasets import load_iris
+
+
+model = joblib.load('model/iris.pkl')
+
+iris_data = load_iris()
+target_names = iris_data.target_names
+
+
+app = FastAPI()
+
+
+class IrisInput(BaseModel):
+    sepal_length: float
+    sepal_width: float
+    petal_length: float
+    petal_width: float
+
+
+# defining endpoint
+@app.post('/predict')
+def predict(iris: IrisInput):
+    data = np.array([[iris.sepal_length, iris.sepal_width, iris.petal_length,
+                      iris.petal_width]])
+    prediction = model.predict(data)[0]
+    class_names = target_names[prediction]
+    return {'prediction': class_names}
+
+
+# uvicorn server:app --reload
